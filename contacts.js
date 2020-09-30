24a74f72e7c8de398dbc49e450bf4f516da79a43
PhilFortunaJr <40374965+PhilFortunaJr@users.noreply.github.com>
2020-09-30 19:12:39 -0400

    Create contacts.js

---
+++ contacts.js
@@ -0,0 +1,13 @@
+var contacts = ["Matt Smith", "Sam Davis", "Ashley Jones"];
+
+var secondContact = contacts[1];
+
+console.log("The second contact in your book is " + secondContact);
+
+contacts.push("Frank Smith");
+
+contacts.pop();
+
+contacts[0] = "Phillip Sanders";
+
+contacts[contacts.length - 1] = "Bill Sanders";
