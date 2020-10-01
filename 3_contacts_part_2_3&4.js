a16a79b2a58b9d405e0dda0652fc01c23d5016e1
PhilFortunaJr <40374965+PhilFortunaJr@users.noreply.github.com>
2020-09-30 20:08:20 -0400

    Update and rename 3_contacts_part_2&3.js to 3_contacts_part_2_3&4.js

--- 3_contacts_part_2_3&4.js
+++ 3_contacts_part_2_3&4.js
@@ -36,3 +36,26 @@ console.log(findContact(2));
 updateLastContact("Patty Davis");
 addContact("Mark Jones");
 console.log(contacts);
+
+
+//Part 4
+
+var contacts = ["Matt Smith", "Sam Davis", "Ashley Jones"];
+
+function findContact (index) {
+    return contacts[index];
+};
+
+function updateLastContact (newName) {
+    contacts[contacts.length - 1] = newName;
+};
+
+function addContact (name) {
+    contacts.push(name);
+};
+
+
+console.log(findContact(2));
+updateLastContact("Patty Davis");
+addContact("Mark Jones");
+console.log(contacts);
